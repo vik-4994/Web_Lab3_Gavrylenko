02fe7216b2c6acb09d585344bfd83c98521df745
vik-4994 <igor.gavrylenko05@gmail.com>
2025-05-24 17:27:37 +0200

    Final

---
+++ src/main/java/org/example/lab3web/Lab3WebApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab3web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3WebApplication.class, args);
+    }
+
+}
